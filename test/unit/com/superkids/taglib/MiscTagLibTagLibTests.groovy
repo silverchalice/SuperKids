93ce802d0a10cd7c237013f5be89fa114f43366a
Zachary Klein <zak@silver-chalice.com>
2010-07-28 11:54:18 -0500

    added MiscTagLib

---
+++ test/unit/com/superkids/taglib/MiscTagLibTagLibTests.groovy
@@ -0,0 +1,17 @@
+package com.superkids.taglib
+
+import grails.test.*
+
+class MiscTagLibTests extends TagLibUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
