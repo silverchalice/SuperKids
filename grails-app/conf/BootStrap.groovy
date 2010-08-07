2d55753810dfef997e7cdb08f619d01410b0f1b0
Zachary Klein <zak@silver-chalice.com>
2010-08-07 11:22:49 -0500

    bootstrap data

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -4,12 +4,9 @@ class BootStrap {
 
     def init = { servletContext ->
 
-
-		def addy = new Address(street1:'61 Harneywold Drive', city:'St Louis', state:'MO', zip:'63136')
-
 		def customer = new Customer(
 			district:'Silver Chalice Schools',
-			address:addy,
+			address:new Address(street1:'61 Harneywold Drive', city:'St Louis', state:'MO', zip:'63136'),
 			phone:'(608) 617-3960',
 			fsdName:'FSD',
 			fsdTitle:'Mr.',
@@ -24,32 +21,61 @@ class BootStrap {
 			purchaseFreshBread: false,
 			purchasePreparedFood: true,
 			purchaseFrozenFood:true
-			)
-
-		def addy2 = new Address(street1:'62 Harneywold Drive', city:'St Louis', state:'MO', zip:'63146')
-
-				def customer2 = new Customer(
-					district:'Green Plate Schools',
-					address:addy2,
-					phone:'(608) 617-3960',
-					fsdName:'FSD',
-					fsdTitle:'Mr.',
-					fsdEmail:'fsd@green-plate.com',
-					studentsInDistrict: '15',
-					facilities: '2',
-					breakfastsServed: '15',
-					lunchesServed: '15',
-					snacksServed:'15',
-					hasBakery: false,
-					purchaseFrozenBread: false,
-					purchaseFreshBread: true,
-					purchasePreparedFood: true,
-					purchaseFrozenFood:true
-					)
+		)
 
 
-		addy.save()
-		addy2.save()
+		def customer2 = new Customer(
+			district:'Green Plate Schools',
+			address:new Address(street1:'62 Harneywold Drive', city:'St Louis', state:'MO', zip:'63146'),
+			phone:'(608) 617-3960',
+			fsdName:'FSD',
+			fsdTitle:'Mr.',
+			fsdEmail:'fsd@green-plate.com',
+			studentsInDistrict: '15',
+			facilities: '2',
+			breakfastsServed: '15',
+			lunchesServed: '15',
+			snacksServed:'15',
+			hasBakery: false,
+			purchaseFrozenBread: false,
+			purchaseFreshBread: true,
+			purchasePreparedFood: true,
+			purchaseFrozenFood:true
+		)
+
+		def sponsor1 = new Sponsor(
+			name:'ACME Foods, Inc',
+			address: new Address(street1:'123 Wall Street', city:'Los Angeles', state: 'CA', zip:'93456'),
+			phone:'1-800 123 4567',
+			website:'www.acmefoods.com',
+			salesContact:'John Doe, jdoe@acmefoods.com 123-456-7890',
+			sampleContact:'Jane Smith jsmith@acmefoods.com 123-456-0987',
+			products:[]
+		).save()
+
+		def product1 = new Product(
+			name:'Healthy Hot Dog',
+			description:'This hot dog is very healthy.',
+			details:'A Healthy Hot Dog is a great way to get healthy hot dogs into your diet.',
+			nutrition:'Fat:0g, Sugar:0g, Protein:0g, Potassium:13g, Uranium:14g',
+			image:new File('/home/zak/builds/SuperKids/web-app/uploads/product1.png').readBytes(),
+			summary:new File('/home/zak/builds/SuperKids/web-app/uploads/product1.pdf').readBytes(),
+			sponsor:sponsor1
+		).save()
+
+		def product2 = new Product(
+			name:'Whole Wheat Waffles',
+			description:'These waffles are made with whole wheat.',
+			details:'Whole Wheat Waffles are great waffles made with whole wheat.',
+			nutrition:'Fat:0g, Sugar:0g, Protein:0g, Lithium:2g, Argon:12g',
+			image:new File('/home/zak/builds/SuperKids/web-app/uploads/product2.png').readBytes(),
+			summary:new File('/home/zak/builds/SuperKids/web-app/uploads/product2.pdf').readBytes(),
+			sponsor:sponsor1
+		).save()
+
+		sponsor1.addToProducts(product1)
+		sponsor1.addToProducts(product2)
+
 		customer.save()
 		customer2.save()
     }
