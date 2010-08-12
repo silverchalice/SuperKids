1c8761d0acaa3a9a9361fb900f637b641bf93e86
Benjamin Klein <ben@silver-chalice.com>
2010-08-12 16:16:48 -0500

    add register action to home controller

--- grails-app/controllers/com/superkids/HomeController.groovy
+++ grails-app/controllers/com/superkids/HomeController.groovy
@@ -1,8 +1,18 @@
 package com.superkids
 
+import com.superkids.domain.Customer
+import com.superkids.domain.Broker
+import com.superkids.domain.Address
+import com.superkids.domain.User
+import com.superkids.domain.UserRole
+import com.superkids.domain.Role
+
 class HomeController {
 
+    def springSecurityService
+
     def index = {
+                println springSecurityService.principal?.username
 		render view:'home'
 	}
 
@@ -13,4 +23,17 @@ class HomeController {
 	def sustagrain = {
 		render view:'sustagrain'
 	}
+
+       def register = {
+            def customerInstance = new Customer()
+            customerInstance.address = new Address()
+            customerInstance.deliveryAddress = new Address()
+            customerInstance.broker = new Broker()
+
+            customerInstance.properties = params
+            
+            return [customerInstance: customerInstance]
+       }
+
+
 }
