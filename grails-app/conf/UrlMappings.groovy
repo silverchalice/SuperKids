dbebad8847f2d3ea1de6394fb375bff071a4c71c
Benjamin Klein <ben@silver-chalice.com>
2010-08-18 14:56:56 -0500

    URL mappings for new broker actions in home controller

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -20,6 +20,30 @@ class UrlMappings {
                     controller = "home"
                     action = "broker"
                 }
+                "/broker-products/" {
+                    controller = "home"
+                    action = "broker_products"
+                }
+                "/broker-whole-grain-benefits/" {
+                    controller = "home"
+                    action = "broker_whole_grain_benefits"
+                }
+                "/broker-whole-grain-studies/" {
+                    controller = "home"
+                    action = "broker_whole_grain_studies"
+                }
+                "/incorporating-whole-grains-in-schools/" {
+                    controller = "home"
+                    action = "incorporating_whole_grains_in_schools"
+                }
+                "/broker-participate/" {
+                    controller = "home"
+                    action = "broker_participate"
+                }
+                "/broker-ecard/" {
+                    controller = "home"
+                    action = "broker_ecard"
+                }
                 "/public/" {
                     controller = "home"
                     action = "anonymous"
