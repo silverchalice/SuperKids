3cb6da5c2fa7e279abb74b8e7378ecdb9ba02658
Benjamin Klein <ben@silver-chalice.com>
2010-08-19 11:03:30 -0500

    add PageText views

---
+++ test/unit/com/superkids/domain/PageTextControllerTests.groovy
@@ -0,0 +1,17 @@
+package com.superkids.domain
+
+import grails.test.*
+
+class PageTextControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
