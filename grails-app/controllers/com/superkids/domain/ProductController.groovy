f9cec67c11ac961891f30de5577196af870e81b1
Benjamin Klein <ben@silver-chalice.com>
2010-08-19 17:44:13 -0500

    shoppingCartService in ProductController

--- grails-app/controllers/com/superkids/domain/ProductController.groovy
+++ grails-app/controllers/com/superkids/domain/ProductController.groovy
@@ -4,6 +4,8 @@ class ProductController {
 
     static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
 
+    def shoppingCartService
+
     def index = {
         redirect(action: "list", params: params)
     }
