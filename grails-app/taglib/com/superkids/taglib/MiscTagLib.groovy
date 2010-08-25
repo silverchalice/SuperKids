6be272785d47b28da55103043240c078596cb926
Benjamin Klein <ben@silver-chalice.com>
2010-08-25 16:24:25 -0500

    add sks:hasPlacedCurrentOrder (MiscTagLib.groovy)

--- grails-app/taglib/com/superkids/taglib/MiscTagLib.groovy
+++ grails-app/taglib/com/superkids/taglib/MiscTagLib.groovy
@@ -1,5 +1,18 @@
 package com.superkids.taglib
 
+import com.superkids.domain.Customer
+
 class MiscTagLib {
+
+    def springSecurityService
+
+    static namespace = 'sks'
+
+    def hasPlacedCurrentOrder = { attrs, body ->
+        def customer = Customer.get(springSecurityService.principal.id)
+        if(customer?.hasPlacedCurrentOrder){
+            out << body()
+        } else { out << "" }
+    }
 
 }
