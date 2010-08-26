c7ae34ac0b2204d9bbd9b0797afdc8fa61a24cf3
Benjamin Klein <ben@silver-chalice.com>
2010-08-26 16:17:11 -0500

    add CustomerOrder class

---
+++ test/unit/com/superkids/domain/CustomerOrderTests.groovy
@@ -0,0 +1,17 @@
+package com.superkids.domain
+
+import grails.test.*
+
+class CustomerOrderTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
