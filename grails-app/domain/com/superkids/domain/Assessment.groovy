b0d4d9784f8e4507a555b2ccbec7b5ae987b0ed5
Benjamin Klein <ben@silver-chalice.com>
2010-09-03 14:44:41 -0500

    rating and comment fields on Assessment: nullable (Assessment.groovy)

--- grails-app/domain/com/superkids/domain/Assessment.groovy
+++ grails-app/domain/com/superkids/domain/Assessment.groovy
@@ -7,14 +7,15 @@ class Assessment {
     String likeComment
     String changeComment
     Product product
+    Boolean completed = false
 
     static belongsTo = [ customer : Customer ]
 
     static constraints = {
-        likeRating nullable:false
-        interestRating: nullable:false
-        likeComment: nullable:false
-        changeComment nullable:false
+        likeRating nullable:true
+        interestRating nullable:true
+        likeComment nullable:true
+        changeComment nullable:true
         product nullable:false
         customer nullable:false
     }
