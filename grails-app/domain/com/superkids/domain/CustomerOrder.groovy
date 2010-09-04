b590176364fa95c7e2d627c43b0c25a18d5b2376
Benjamin Klein <ben@silver-chalice.com>
2010-09-04 14:00:22 -0500

    add shippingDate to CustomerOrder

--- grails-app/domain/com/superkids/domain/CustomerOrder.groovy
+++ grails-app/domain/com/superkids/domain/CustomerOrder.groovy
@@ -7,11 +7,13 @@ import com.superkids.domain.OrderType
 class CustomerOrder {
     Customer customer
     Date dateCreated
+    Date shippingDate
     OrderType orderType
 
     static hasMany = [products:Product]
     static belongsTo = Customer
 
     static constraints = {   
+        shippingDate nullable:true
     }
 }
