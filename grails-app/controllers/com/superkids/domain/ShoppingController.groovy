0b683b450e0679dd4a319ca3bd46ca73db942969
Benjamin Klein <ben@silver-chalice.com>
2010-09-10 17:44:53 -0500

    add confirm action to ShoppingController

--- grails-app/controllers/com/superkids/domain/ShoppingController.groovy
+++ grails-app/controllers/com/superkids/domain/ShoppingController.groovy
@@ -2,5 +2,32 @@ package com.superkids.domain
 
 class ShoppingController {
 
-    def index = { }
+    def index = { 
+        
+    }
+
+    def confirm = { 
+       def customerInstance = Customer.get(params.id)
+       if (customerInstance) {
+           if (params.version) {
+               def version = params.version.toLong()
+               if (customerInstance.version > version) {
+                   customerInstance.errors.rejectValue("version", "default.optimistic.locking.failure", [message(code: 'customer.label', default: 'Customer')] as Object[], "Another user has updated this Customer while you were editing")
+                   render(view: "check_out", model: [customerInstance: customerInstance])
+                   return
+               }
+           }
+           customerInstance.properties = params
+           if (!customerInstance.hasErrors() && customerInstance.save(flush: true)) {
+               flash.message = "w00t! we updated the Customer!"
+               redirect(controller:"home", action: "index")
+           } else {
+               render(view: "checkout", model: [customerInstance: customerInstance])
+           }
+       } else {
+           flash.message = "Couldn't find that customer record."
+           redirect(controller:"home", action: "index")
+       }
+    }
+
 }
