2a11272f2c8910368d4f16cd84a2a00857a77268
Benjamin Klein <ben@silver-chalice.com>
2010-09-17 23:38:06 -0500

    set up home contact process: save new contactRequest, then redirect to home index (ContactRequestController.groovy)

--- grails-app/controllers/com/superkids/domain/ContactRequestController.groovy
+++ grails-app/controllers/com/superkids/domain/ContactRequestController.groovy
@@ -17,15 +17,23 @@ class ContactRequestController {
 
     def create = {
         def contactRequestInstance = new ContactRequest()
+        def states=['AL': 'Alabama', 'AK': 'Alaska', 'AZ': 'Arizona', 'AR': 'Arkansas', 'CA': 'California', 'CO': 'Colorado', 'CT': 'Connecticut', 'DE': 'Delaware', 'DC': 'District Of Columbia', 'FL': 'Florida', 'GA': 'Georgia', 'HI': 'Hawaii', 'ID': 'Idaho', 'IL': 'Illinois', 'IN': 'Indiana', 'IA': 'Iowa', 'KS': 'Kansas', 'KY': 'Kentucky', 'LA': 'Louisiana', 'ME': 'Maine', 'MD': 'Maryland', 'MA': 'Massachusetts', 'MI': 'Michigan', 'MN': 'Minnesota', 'MS': 'Mississippi', 'MO': 'Missouri', 'MT': 'Montana', 'NE': 'Nebraska', 'NV': 'Nevada', 'NH': 'New Hampshire', 'NJ': 'New Jersey', 'NM': 'New Mexico', 'NY': 'New York', 'NC': 'North Carolina', 'ND': 'North Dakota', 'OH': 'Ohio', 'OK': 'Oklahoma', 'OR': 'Oregon', 'PA': 'Pennsylvania', 'RI': 'Rhode Island', 'SC': 'South Carolina', 'SD': 'South Dakota', 'TN': 'Tennessee', 'TX': 'Texas', 'UT': 'Utah', 'VT': 'Vermont', 'VA': 'Virginia', 'WA': 'Washington', 'WV': 'West Virginia', 'WI': 'Wisconsin', 'WY': 'Wyoming']
         contactRequestInstance.properties = params
-        return [contactRequestInstance: contactRequestInstance]
+        return [contactRequestInstance: contactRequestInstance, states:states]
     }
 
     def save = {
+        def address = new Address(street:params.street, street2:params.street2, city:params.city, state:params.address.state, zip:params.zip)
+        address.save(failOnError:true)
+
         def contactRequestInstance = new ContactRequest(params)
+
+        contactRequestInstance.address = address
+        contactRequestInstance.phone = params.phone1 + " " + params.phone2 + " " + params.phone3
+
         if (contactRequestInstance.save(flush: true)) {
-            flash.message = "${message(code: 'default.created.message', args: [message(code: 'contactRequest.label', default: 'ContactRequest'), contactRequestInstance.id])}"
-            redirect(action: "show", id: contactRequestInstance.id)
+            flash.message = "Thanks! Your message has been sent."
+            redirect controller:"home", action:"index"
         }
         else {
             render(view: "create", model: [contactRequestInstance: contactRequestInstance])
