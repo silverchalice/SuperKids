dfbf6a0e744a7b5a7d8b8304017c4c67590e6855
Benjamin Klein <ben@silver-chalice.com>
2010-09-22 22:27:24 -0500

    back to .create

--- grails-app/services/com/superkids/DataService.groovy
+++ grails-app/services/com/superkids/DataService.groovy
@@ -58,10 +58,7 @@ class DataService {
 				customer.errors.each {println it}
 			}
                         def userRole = Role.findByAuthority("ROLE_USER")
-                        def ur = new UserRole(customer, userRole)
-                        if(!ur.save()){
-                                ur.errors.each { println it }
-                        }
+                        UserRole.create customer, userRole, true
 		}
 	}
 }
