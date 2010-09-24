5612031bbec2fbbead220e2254bdb8bb83412329
Benjamin Klein <ben@silver-chalice.com>
2010-09-24 16:13:16 -0500

    name -> firstName and lastName; add email (Admin.groovy)

--- grails-app/domain/com/superkids/domain/Admin.groovy
+++ grails-app/domain/com/superkids/domain/Admin.groovy
@@ -2,10 +2,12 @@ package com.superkids.domain
 
 class Admin extends User {
 
-    String name
+    String firstName
+    String lastName
     Date dateCreated
     Date lastUpdated
     Date lastLogin
+    String email
 
     static constraints = {
         lastLogin nullable:true
