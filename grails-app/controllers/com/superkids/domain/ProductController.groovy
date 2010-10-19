7a550172a993961431243822082f1ce1622b71b9
Benjamin Klein <ben@silver-chalice.com>
2010-10-19 13:01:43 -0500

    return states in product edit (ProductController.groovy)

--- grails-app/controllers/com/superkids/domain/ProductController.groovy
+++ grails-app/controllers/com/superkids/domain/ProductController.groovy
@@ -62,6 +62,16 @@ class ProductController {
     }
 
     def edit = {
+        def states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+                      'Colorado', 'Connecticut', 'Delaware', 'District of Columbia',
+                      'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana',
+                      'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland',
+                      'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri',
+                      'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey',
+                      'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio',
+                      'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina',
+                      'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia',
+                      'Virgin Islands', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
         def productInstance = Product.get(params.id)
         if (!productInstance) {
             flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'product.label', default: 'Product'), params.id])}"
@@ -75,7 +85,7 @@ class ProductController {
 				println "There are sub products"
 			}
 
-            return [productInstance: productInstance, subProducts: subProducts]
+            return [productInstance: productInstance, subProducts: subProducts, states:states]
         }
     }
 
