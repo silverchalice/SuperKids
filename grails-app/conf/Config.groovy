08ac83144d8608f61516a6aa1586414546aaac5c
Benjamin Klein <ben@silver-chalice.com>
2010-10-21 14:03:40 -0500

    c_forgot_password, c_passwd_reset, and c_reset_success: open to anonymous users (Config.groovy)

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -223,6 +223,9 @@ grails.plugins.springsecurity.interceptUrlMap = [
    '/product/displayImage/**':                           ['IS_AUTHENTICATED_ANONYMOUSLY'],
    '/sponsor/displayImage/**':                           ['IS_AUTHENTICATED_ANONYMOUSLY'],
    '/contactRequest/save':                               ['IS_AUTHENTICATED_ANONYMOUSLY'],
+   '/c_forgot_password':                                 ['IS_AUTHENTICATED_ANONYMOUSLY'],
+   '/c_passwd_reset':                                    ['IS_AUTHENTICATED_ANONYMOUSLY'],
+   '/c_reset_success':                                    ['IS_AUTHENTICATED_ANONYMOUSLY'],
    '/**':                                                                  ['ROLE_ADMIN']
 ]
 
