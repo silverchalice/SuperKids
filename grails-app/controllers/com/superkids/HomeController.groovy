e5a4586d31711f8b1b8e397072bc66cfb333fd89
Benjamin Klein <ben@silver-chalice.com>
2010-10-23 21:49:41 -0500

    don't save broker separately in home save (HomeController.groovy)

--- grails-app/controllers/com/superkids/HomeController.groovy
+++ grails-app/controllers/com/superkids/HomeController.groovy
@@ -89,7 +89,6 @@ class HomeController {
                UserRole.create customerInstance, userRole, true
                if(params.brokerName){
                    def broker = new Broker(name:params.brokerName, phone:params.brokerPhone, fax:params.brokerFax, email:params.brokerEmail, street:params.brokerStreet, street2:params.brokerStreet2, city:params.brokerCity, state:params.brokerState, zip:params.brokerZip, customer:customerInstance)
-                   broker.save(failOnError:true)
                    println broker.name
                    customerInstance.addToBrokers(broker)
                    customerInstance.save()
