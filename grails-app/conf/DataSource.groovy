99ff1879c8ef4e60ef61066aabedc135aaad058a
Zachary Klein <zak@silver-chalice.com>
2012-09-23 22:15:22 -0500

    Datasource fix

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -32,12 +32,7 @@ environments {
 			password = "sks5551212"
 			dbCreate = "update"			
             url = "jdbc:mysql://localhost:3306/sksdb"
-			dialect=org.hibernate.dialect.MySQLInnoDBDialect
-			properties {
-				validationQuery="select 1"
-				testWhileIdle=true
-				timeBetweenEvictionRunsMillis=60000
-			}
+
         }
     }
 }
