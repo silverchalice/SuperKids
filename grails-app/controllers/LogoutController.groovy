461349ffd1655846ad0e37c0c8664c0d32d5ba05
Zachary Klein <zak@silver-chalice.com>
2012-10-15 23:14:10 -0500

    Added demo user support

--- grails-app/controllers/LogoutController.groovy
+++ grails-app/controllers/LogoutController.groovy
@@ -1,15 +1,47 @@
 import org.codehaus.groovy.grails.plugins.springsecurity.SpringSecurityUtils
+import com.superkids.domain.Customer
+import com.superkids.domain.CustomerStatus
 
 class LogoutController {
 
 	/**
 	 * Index action. Redirects to the Spring security logout uri.
 	 */
-	def index = {
+    def springSecurityService
 
-		println "entering login:index"
+        def index = {
 
-		// TODO  put any pre-logout code here
+            println "entering login:index"
+            def user = Customer.get(springSecurityService.principal.id)
+
+            if(user && user.email == 'demo') {
+                println "found demo user..."
+                user.with {
+                    status = CustomerStatus.HAS_NOT_ORDERED
+                    hasPlacedCurrentOrder = false
+                    hasCompletedCurrentAssessment = false
+                }
+
+                user.assessments.each { assessment ->
+                    user.removeFromAssessments(assessment)
+                    assessment.delete()
+                }
+
+                def order = user.order
+
+                if(order) {
+                    user.order = null
+                    order.delete()
+                }
+
+                if(!user.save()) {
+                    user.errors.allErrors.each { println it }
+                }
+
+            }
+
+     // TODO  put any pre-logout code here
 		redirect uri: SpringSecurityUtils.securityConfig.logout.filterProcessesUrl // '/j_spring_security_logout'
+
 	}
 }
