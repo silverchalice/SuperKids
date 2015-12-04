609df94036a67f8bc3db4baaad833150dbd90f96
Zachary Klein <zak@silver-chalice.com>
2015-12-04 02:34:01 -0600

    debuf

--- grails-app/controllers/com/superkids/domain/ReportController.groovy
+++ grails-app/controllers/com/superkids/domain/ReportController.groovy
@@ -154,34 +154,50 @@ class ReportController {
                 if (withAssessments == 'true') {
 
                     for (prod in prods) {
-                        if (prod.id != 38 && prod.id != 39) {
-                            Assessment assessment = customer?.assessments?.find { it?.product?.id == prod?.id }
+                        Assessment assessment = customer?.assessments?.find { it?.product?.id == prod?.id }
 
-                            def orderedProduct = customer?.customerOrder?.products?.find { it?.product?.id == prod.id }
+                        def orderedProduct = customer?.customerOrder?.products?.find { it?.product?.id == prod.id }
 
-                            if (orderedProduct) {
-                                if (!orderedProduct?.received) {
-                                    m."${prod.name}_Q1" = "Did Not Receive"
-                                    m."${prod.name}_Q2" = "Did Not Receive"
-                                    m."${prod.name}_Q3" = "Did Not Receive"
-                                } else if (!orderedProduct?.sampled) {
-                                    m."${prod.name}_Q1" = "Did Not Sample"
-                                    m."${prod.name}_Q2" = "Did Not Sample"
-                                    m."${prod.name}_Q3" = "Did Not Sample"
-                                } else if (assessment) {
-                                    m."${prod.name}_Q1" = assessment?.likeRating
-                                    m."${prod.name}_Q2" = assessment?.likeComment
-                                    m."${prod.name}_Q3" = assessment?.changeComment
-                                    m.type = assessment.type
-                                    /*if(prod?.id == 23) {
-                                      m."${prod.name}_Q4" = assessment.favorite
-                                    }*/
-                                } else {
-                                    println "something when wrong here..."
-                                    println "(the prod.id was ${prod?.id})"
+
+                        if(prod?.id?.toInteger() == 76) {
+                            println "This is the one!"
+
+                            if(orderedProduct) {
+                                println "They got this one!"
+
+                                if(assessment) {
+                                    println "They assessed this one!"
+                                    println "likeRating:" + assessment?.likeRating
+                                    println "likeComment:" + assessment?.likeComment
+                                    println "changeComment:" + assessment?.changeComment
+
                                 }
                             }
                         }
+
+                        if (orderedProduct) {
+                            if (!orderedProduct?.received) {
+                                m."${prod.name}_Q1" = "Did Not Receive"
+                                m."${prod.name}_Q2" = "Did Not Receive"
+                                m."${prod.name}_Q3" = "Did Not Receive"
+                            } else if (!orderedProduct?.sampled) {
+                                m."${prod.name}_Q1" = "Did Not Sample"
+                                m."${prod.name}_Q2" = "Did Not Sample"
+                                m."${prod.name}_Q3" = "Did Not Sample"
+                            } else if (assessment) {
+                                m."${prod.name}_Q1" = assessment?.likeRating
+                                m."${prod.name}_Q2" = assessment?.likeComment
+                                m."${prod.name}_Q3" = assessment?.changeComment
+                                m.type = assessment.type
+                                /*if(prod?.id == 23) {
+                                  m."${prod.name}_Q4" = assessment.favorite
+                                }*/
+                            } else {
+                                println "something when wrong here..."
+                                println "(the prod.id was ${prod?.id})"
+                            }
+                        }
+
                     }
 
                     def rA = customer?.assessments?.find { it }
