0200aed70182cd2bdd2e73c006ea124f6139ba9a
Zachary Klein <zak@silver-chalice.com>
2017-10-31 01:30:06 -0700

    Fixed typo

--- grails-app/controllers/com/superkids/domain/ProductController.groovy
+++ grails-app/controllers/com/superkids/domain/ProductController.groovy
@@ -6,7 +6,7 @@ class ProductController {
 
     static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
 
-    static def states = ['AL','AK','AZ','AR',' CA','CO','CT','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME',
+    static def states = ['AL','AK','AZ','AR','CA','CO','CT','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME',
             'MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA',
             'RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY']
 
